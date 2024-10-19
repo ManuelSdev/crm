b5502d302b96d716a742cb58a48450d64ae44eb3
manuelsdev <masanchezzm@gmail.com>
2024-10-19 11:47:47 +0200

    *

--- src/layouts/DashBoardLayout.jsx
+++ src/layouts/DashBoardLayout.jsx
@@ -2,29 +2,16 @@
 import * as React from 'react'
 import { styled, useTheme } from '@mui/material/styles'
 import Box from '@mui/material/Box'
-import MuiDrawer from '@mui/material/Drawer'
 import MuiAppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
-import List from '@mui/material/List'
+
 import CssBaseline from '@mui/material/CssBaseline'
 import Typography from '@mui/material/Typography'
-import Divider from '@mui/material/Divider'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
-import ChevronRightIcon from '@mui/icons-material/ChevronRight'
-import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import ListItemText from '@mui/material/ListItemText'
-import InboxIcon from '@mui/icons-material/MoveToInbox'
-import MailIcon from '@mui/icons-material/Mail'
-import MenuOpenIcon from '@mui/icons-material/MenuOpen'
-import DesktopDrawer from '@/components/drawer/DesktopDrawer'
+
 import { drawerContent } from '@/components/drawer/drawerContent'
-import MobileDrawer from '@/components/drawer/MobileDrawer'
-import useMediaQuery from '@mui/material/useMediaQuery'
+
 import Drawer from '@/components/drawer/Drawer'
+import Header from '@/components/header/Header'
+import { Container } from '@mui/material'
 
 const drawerWidth = 240
 
@@ -35,29 +22,6 @@ const DrawerHeader = styled('div')(({ theme }) => ({
    padding: theme.spacing(0, 1),
    // necessary for content to be below app bar
    ...theme.mixins.toolbar,
-}))
-
-const AppBar = styled(MuiAppBar, {
-   shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme }) => ({
-   zIndex: theme.zIndex.drawer + 1,
-   transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-   }),
-   variants: [
-      {
-         props: ({ open }) => open,
-         style: {
-            marginLeft: drawerWidth,
-            width: `calc(100% - ${drawerWidth}px)`,
-            transition: theme.transitions.create(['width', 'margin'], {
-               easing: theme.transitions.easing.sharp,
-               duration: theme.transitions.duration.enteringScreen,
-            }),
-         },
-      },
-   ],
 }))
 
 export default function DashBoardLayout() {
@@ -93,59 +57,14 @@ export default function DashBoardLayout() {
    return (
       <Box sx={{ display: 'flex' }}>
          <CssBaseline />
-         <AppBar position="fixed">
-            <Toolbar>
-               <IconButton
-                  color="inherit"
-                  aria-label="open drawer"
-                  edge="start"
-                  onClick={handleDrawerToggle}
-                  sx={{ mr: 2, display: { xs: 'block', sm: 'none' } }}
-               >
-                  <MenuIcon />
-               </IconButton>
-               {open || (
-                  <IconButton
-                     color="inherit"
-                     aria-label="open drawer"
-                     onClick={handleDesktopDrawerOpen}
-                     edge="start"
-                     sx={[
-                        {
-                           marginRight: 5,
-                           display: { xs: 'none', sm: 'inline-flex' },
-                        },
-                        // { display: { xs: 'none', sm: 'block' } },
-                        // open && { display: 'none' },
-                     ]}
-                  >
-                     <MenuIcon />
-                  </IconButton>
-               )}
+         <Header
+            drawerWidth={drawerWidth}
+            open={open}
+            handleDesktopDrawerOpen={handleDesktopDrawerOpen}
+            handleDesktopDrawerClose={handleDesktopDrawerClose}
+            handleDrawerToggle={handleDrawerToggle}
+         />
 
-               {open && (
-                  <IconButton
-                     color="inherit"
-                     aria-label="open drawer"
-                     onClick={handleDesktopDrawerClose}
-                     edge="start"
-                     sx={[
-                        {
-                           // display: { xs: 'none', sm: 'block' },
-                           marginRight: 5,
-                        },
-                        { display: { xs: 'none', sm: 'inline-flex' } },
-                        //open || { display: 'none' },
-                     ]}
-                  >
-                     <MenuOpenIcon />
-                  </IconButton>
-               )}
-               <Typography variant="h6" noWrap component="div">
-                  Mini variant drawer
-               </Typography>
-            </Toolbar>
-         </AppBar>
          <Drawer
             open={open}
             drawerWidth={drawerWidth}
@@ -160,132 +79,7 @@ export default function DashBoardLayout() {
             handleDrawerToggle={handleDrawerToggle}
          />
 
-         {/*  <Drawer
-            variant="permanent"
-            open={open}
-            sx={{ display: { xs: 'none', sm: 'block' } }}
-         >
-            <DrawerHeader>
-               <IconButton onClick={handleDrawerClose}>
-                  {theme.direction === 'rtl' ? (
-                     <ChevronRightIcon />
-                  ) : (
-                     <ChevronLeftIcon />
-                  )}
-               </IconButton>
-            </DrawerHeader>
-            <Divider />
-            <List>
-               {['Inbox', 'Starred', 'Send email', 'Drafts'].map(
-                  (text, index) => (
-                     <ListItem
-                        key={text}
-                        disablePadding
-                        sx={{ display: 'block' }}
-                     >
-                        <ListItemButton
-                           sx={[
-                              {
-                                 minHeight: 48,
-                                 px: 2.5,
-                              },
-                              open
-                                 ? {
-                                      justifyContent: 'initial',
-                                   }
-                                 : {
-                                      justifyContent: 'center',
-                                   },
-                           ]}
-                        >
-                           <ListItemIcon
-                              sx={[
-                                 {
-                                    minWidth: 0,
-                                    justifyContent: 'center',
-                                 },
-                                 open
-                                    ? {
-                                         mr: 3,
-                                      }
-                                    : {
-                                         mr: 'auto',
-                                      },
-                              ]}
-                           >
-                              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                           </ListItemIcon>
-                           <ListItemText
-                              primary={text}
-                              sx={[
-                                 open
-                                    ? {
-                                         opacity: 1,
-                                      }
-                                    : {
-                                         opacity: 0,
-                                      },
-                              ]}
-                           />
-                        </ListItemButton>
-                     </ListItem>
-                  )
-               )}
-            </List>
-            <Divider />
-            <List>
-               {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                  <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-                     <ListItemButton
-                        sx={[
-                           {
-                              minHeight: 48,
-                              px: 2.5,
-                           },
-                           open
-                              ? {
-                                   justifyContent: 'initial',
-                                }
-                              : {
-                                   justifyContent: 'center',
-                                },
-                        ]}
-                     >
-                        <ListItemIcon
-                           sx={[
-                              {
-                                 minWidth: 0,
-                                 justifyContent: 'center',
-                              },
-                              open
-                                 ? {
-                                      mr: 3,
-                                   }
-                                 : {
-                                      mr: 'auto',
-                                   },
-                           ]}
-                        >
-                           {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                        </ListItemIcon>
-                        <ListItemText
-                           primary={text}
-                           sx={[
-                              open
-                                 ? {
-                                      opacity: 1,
-                                   }
-                                 : {
-                                      opacity: 0,
-                                   },
-                           ]}
-                        />
-                     </ListItemButton>
-                  </ListItem>
-               ))}
-            </List>
-         </Drawer> */}
-         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+         <Box component="main" sx={{ flexGrow: 1, p: 4 }}>
             <DrawerHeader />
             <Typography sx={{ marginBottom: 2 }}>
                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
